69a70e1c01d04f5da37171979cb83952f3ea402e
Michael Schmeißer <misc@skamandros.de>
2025-01-25 00:19:50 +0100

    Added speed property to the frog

--- core/src/main/java/github/team42/ggj25/entity/Frog.java
+++ core/src/main/java/github/team42/ggj25/entity/Frog.java
@@ -7,8 +7,18 @@ import github.team42.ggj25.Constants;
  * Our frog.
   */
 public class Frog extends Entity {
+    public static final float BASE_SPEED = 1f;
+    private float speed = BASE_SPEED;
 
     public Frog(){
         super("frog.png", new Rectangle(Constants.WIDTH / 2f, Constants.HEIGHT / 2f, 96, 54));
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
     }
 }
