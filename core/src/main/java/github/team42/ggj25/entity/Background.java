e158b8593cec2fd037d152b5e7077fc63cd0c60f
Roman Kempt <roman.kempt@gmail.com>
2025-01-24 23:52:49 +0100

    added background

--- core/src/main/java/github/team42/ggj25/entity/Background.java
+++ core/src/main/java/github/team42/ggj25/entity/Background.java
@@ -1,14 +1,40 @@
 package github.team42.ggj25.entity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import github.team42.ggj25.Constants;
 import github.team42.ggj25.Drawable;
 
 /**
  * The game background, i.e. water and the pike.
  */
 public class Background implements Drawable {
+
+    private final Texture m_blue_background;
+    private final Texture m_water;
+    private final Texture m_water_dark;
+    private final Texture m_water_with_white_puddle;
+
+    public Background() {
+        FileHandle blue_background = Gdx.files.internal("blue_background.png");
+        FileHandle water = Gdx.files.internal("water.png");
+        FileHandle water_dark = Gdx.files.internal("water_dark.png");
+        FileHandle water_with_white_puddle = Gdx.files.internal("water_with_white_puddle.png");
+
+        m_blue_background = new Texture(blue_background);
+        m_water = new Texture(water);
+        m_water_dark = new Texture(water_dark);
+        m_water_with_white_puddle = new Texture(water_with_white_puddle);
+    }
+
     @Override
     public void draw(SpriteBatch spriteBatch) {
-
+        // store the worldWidth and worldHeight as local variables for brevity
+        spriteBatch.draw(m_blue_background, 0, 0, Constants.WIDTH, Constants.HEIGHT);
+        spriteBatch.draw(m_water, 0, 0, Constants.WIDTH, Constants.HEIGHT);
+        spriteBatch.draw(m_water_dark, 0, 0, Constants.WIDTH, Constants.HEIGHT);
+        spriteBatch.draw(m_water_with_white_puddle, 0, 0, Constants.WIDTH, Constants.HEIGHT);
     }
 }
--- core/src/main/java/github/team42/ggj25/screen/GameScreen.java
+++ core/src/main/java/github/team42/ggj25/screen/GameScreen.java
@@ -1,20 +1,28 @@
 package github.team42.ggj25.screen;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import github.team42.ggj25.Constants;
 import github.team42.ggj25.gamestate.GameState;
 
 public class GameScreen extends ScreenAdapter {
     private final SpriteBatch batch= new SpriteBatch();
     private final Texture image = new Texture("libgdx.png");
     private final GameState gameState = new GameState();
+    private final Camera camera  = new PerspectiveCamera();;
+    private final FitViewport viewport = new FitViewport(Constants.WIDTH, Constants.HEIGHT, camera);
 
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        viewport.apply();
+        //batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
@@ -27,5 +35,10 @@ public class GameScreen extends ScreenAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 }
