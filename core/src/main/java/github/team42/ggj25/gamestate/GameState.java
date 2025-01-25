b317fd0a73443504e6aba6513fb7fe4601c85cbf
IP1337 <marius.ms.schneider@gmail.com>
2025-01-25 11:23:45 +0100

    Pike is now roaming the pond and can make the player lose with an attack.

--- core/src/main/java/github/team42/ggj25/gamestate/GameState.java
+++ core/src/main/java/github/team42/ggj25/gamestate/GameState.java
@@ -12,8 +12,10 @@ public class GameState implements Drawable {
     private final List<Enemy> enemies = new ArrayList<>();
     private final Background background = new Background();
     private final Leaf leaf = new Leaf();
+    private final Pike pike = new Pike();
     private final ScoreBoard scoreBoard = new ScoreBoard();
     private final List<Projectile> activeProjectiles = new ArrayList<>();
+    boolean lost = false;
 
     private final int bonusPoints = 3;
     private final float bonusPointsInterval = 1;
@@ -21,28 +23,36 @@ public class GameState implements Drawable {
 
     @Override
     public void update(float delta) {
-        background.update(delta);
-        leaf.update(delta);
-        for (final Enemy enemy : this.enemies) {
-            enemy.update(delta);
-        }
-        player.update(delta);
-        for (Projectile p : activeProjectiles) {
-            p.update(delta);
-        }
-        activeProjectiles.removeIf(p -> !p.isActive());
+        if (!lost) {
+            background.update(delta);
+            pike.update(delta);
+            leaf.update(delta);
+            for (final Enemy enemy : this.enemies) {
+                enemy.update(delta);
+            }
+            player.update(delta);
+            for (Projectile p : activeProjectiles) {
+                p.update(delta);
+            }
+            activeProjectiles.removeIf(p -> !p.isActive());
 
-        bonusPointCooldown -= delta;
-        if (bonusPointCooldown <= 0) {
-            bonusPointCooldown = bonusPointsInterval;
-            scoreBoard.addPointsToScore(bonusPoints);
+            bonusPointCooldown -= delta;
+            if (bonusPointCooldown <= 0) {
+                bonusPointCooldown = bonusPointsInterval;
+                scoreBoard.addPointsToScore(bonusPoints);
+            }
+            scoreBoard.update(delta);
+
+            if (player.overlapsWith(pike) && !pike.getIsPreparingToAttack()) {
+                lost = true;
+            }
         }
-        scoreBoard.update(delta);
     }
 
     @Override
     public void draw(SpriteBatch spriteBatch) {
         background.draw(spriteBatch);
+        pike.draw(spriteBatch);
         leaf.draw(spriteBatch);
         for (final Enemy enemy : this.enemies) {
             enemy.draw(spriteBatch);
