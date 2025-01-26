b41ac7a755ed1719908159584422f4798f06837e
Merando <merando@gmx.de>
2025-01-26 17:57:52 +0100

    added Press A and S added, placing fixed.

--- core/src/main/java/github/team42/ggj25/entity/SkillScreen.java
+++ core/src/main/java/github/team42/ggj25/entity/SkillScreen.java
@@ -2,10 +2,14 @@ package github.team42.ggj25.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 import github.team42.ggj25.Constants;
 import github.team42.ggj25.Drawable;
@@ -20,6 +24,13 @@ public class SkillScreen implements Drawable, Disposable {
     private final TextureAtlas frogJump;
     private float stateTime = 0f;
 
+    private final Label pressALabel;
+    private final Label.LabelStyle pressAStyle = new Label.LabelStyle();
+    private final Label pressSLabel;
+    private final Label.LabelStyle pressSStyle = new Label.LabelStyle();
+    private final int row_height = Constants.HEIGHT / 36;
+    private final int textPadding = row_height / 2;
+
     private final int sideOffset = 300;
     private Animation<TextureAtlas.AtlasRegion> animation;
 
@@ -27,6 +38,24 @@ public class SkillScreen implements Drawable, Disposable {
         FileHandle skillScreenFile = Gdx.files.internal("menu_background.png");
         skillScreenTexture = new Texture(skillScreenFile);
         frogJump = new TextureAtlas(Gdx.files.internal("frog/animation_frogjump.txt"));
+
+        pressAStyle.font = new BitmapFont(Gdx.files.internal("font.fnt"));
+        pressAStyle.fontColor = Color.GREEN;
+
+        pressSStyle.font = new BitmapFont(Gdx.files.internal("font.fnt"));
+        pressSStyle.fontColor = Color.GREEN;
+
+        pressALabel = new Label("PRESS A", pressAStyle);
+        pressALabel.setSize(Gdx.graphics.getWidth(), row_height);
+        pressALabel.setPosition(-220, (float) (Constants.HEIGHT) / 2 - 250);
+        pressALabel.setAlignment(Align.center);
+        pressALabel.setFontScale(2.5f);
+
+        pressSLabel = new Label("PRESS S", pressSStyle);
+        pressSLabel.setSize(Gdx.graphics.getWidth(), row_height);
+        pressSLabel.setPosition(220 , (float) (Constants.HEIGHT) / 2 - 250);
+        pressSLabel.setAlignment(Align.center);
+        pressSLabel.setFontScale(2.5f);
     }
 
     public void init() {
@@ -43,14 +72,32 @@ public class SkillScreen implements Drawable, Disposable {
         spriteBatch.draw(this.animation.getKeyFrame(stateTime), 0, 0, Constants.WIDTH, Constants.HEIGHT);
         spriteBatch.draw(skillScreenTexture, 0, 0, Constants.WIDTH, Constants.HEIGHT);
 
-        for (int i = 0; i < skillTrees.size(); i++) {
+        // TODO: fix constant magic
+        spriteBatch.draw(
+            skillTrees.get(0).getIconTexture(),
+            sideOffset + 220,
+            (float) (Constants.HEIGHT) / 2 - 200,
+            420,
+            420);
+
+        spriteBatch.draw(
+            skillTrees.get(0).getIconTexture(),
+            sideOffset + 220 + 400,
+            (float) (Constants.HEIGHT) / 2 - 200,
+            420,
+            420);
+
+        pressALabel.draw(spriteBatch, 1);
+        pressSLabel.draw(spriteBatch, 1);
+
+        /*for (int i = 0; i < skillTrees.size(); i++) {
             spriteBatch.draw(
                 skillTrees.get(i).getIconTexture(),
                 sideOffset + i * 420,
                 (float) (Constants.HEIGHT) / 2 - 200,
                 420,
                 420);
-        }
+        }*/
     }
 
     @Override
